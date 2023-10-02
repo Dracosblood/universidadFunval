d6b256a327c7f86aa8fd971715497df35487bfb0
darwin <djtb1994iut@gmail.com>
2023-10-02 14:06:33 -0400

    creamos nuestra conexion con talwindcss para los estilos

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        logo: {
+          DEFAULT: '#fff5d2',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
+// fff5d2
